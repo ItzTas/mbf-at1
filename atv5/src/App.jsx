81c33c021af9ce55d3b039ffc4bfdec4c6b9f2a3
ItzTas <talessabini2@gmail.com>
2024-09-24 21:29:39 -0400

    atividade 5

---
+++ atv5/src/App.jsx
@@ -0,0 +1,18 @@
+import styles from './App.module.css';
+
+function App() {
+    return (
+        <div className={styles.app}>
+            <header className={styles.header}>Header</header>
+            <nav className={styles.gMenu}>
+                <span className={styles.gMenuText}>Global Menu</span>
+            </nav>
+            <aside className={styles.ads}>Ads</aside>
+            <aside className={styles.cMenu}>Context Menu</aside>
+            <main className={styles.mContent}>Main Content</main>
+            <footer className={styles.footer}>Footer</footer>
+        </div>
+    );
+}
+
+export default App;
